3b1d8be5112af219aaf01beaa5f4af95a7b35b3a
Nick DeRobertis <whoopnip@gmail.com>
2021-05-02 14:25:21 -0400

    Remove unnecessary JS scripts

--- webapp/src/js/hotjar.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function() {
-  if (env.CF_ANALYTICS_HOTJAR) {
-    var head = document.getElementsByTagName("head")[0];
-    var js = document.createElement("script");
-    js.type = "text/javascript";
-    js.src = "hotjar.js";
-    head.appendChild(js);
-  }
-});
